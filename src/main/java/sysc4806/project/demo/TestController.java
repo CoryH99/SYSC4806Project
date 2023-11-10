39c63f607aed9e0cbc281e42214b5695d39d6b4e
Giancarlo Salvador <50672233+acegiansal@users.noreply.github.com>
2023-11-10 13:59:06 -0500

    Added Student object, fixed Project object by making restrictions a list, added more debug code

--- src/main/java/sysc4806/project/demo/TestController.java
+++ src/main/java/sysc4806/project/demo/TestController.java
@@ -1,17 +1,22 @@
 package sysc4806.project.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TestController {
     @Autowired
     private ProjectRepository repo;
+
+    @Autowired
+    private StudentRepository studRepo;
 
     @PostMapping("/testProj")
     public Project createProj(@RequestBody Project proj){
@@ -21,6 +26,37 @@ public class TestController {
     @GetMapping("/getProj")
     public List<Project> getProjects(){
         return repo.findAll();
+    }
+
+    @GetMapping("/getStudents")
+    public List<Student> getStudents(){ return studRepo.findAll();}
+
+    @PostMapping("/createStudent")
+    public Student createStudent(@RequestBody Student student){
+        return studRepo.save(student);
+    }
+
+    @PutMapping("/assignProject")
+    public ResponseEntity<Student> assignProject(@RequestParam Long studId, @RequestParam Long projId){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        Student stud;
+        Project proj;
+
+
+        Optional<Student> target = studRepo.findById(studId);
+
+        if (target.isPresent()){
+            stud = target.get();
+            Optional<Project> projTarget = repo.findById(projId);
+
+            if(projTarget.isPresent()){
+                proj = projTarget.get();
+                stud.setProject(proj);
+                return new ResponseEntity<Student>(studRepo.save(stud), headers, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<Student>(null, headers, HttpStatus.NOT_FOUND);
     }
 
 }
