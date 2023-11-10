1c95b297947a092f4c216edd1f5a463ad6015512
Jackie Zhuang <jackie_prime@hotmail.ca>
2023-11-10 16:13:28 -0500

    Added student controller

--- src/main/java/sysc4806/project/demo/StudentController.java
+++ src/main/java/sysc4806/project/demo/StudentController.java
@@ -12,13 +12,13 @@ public class StudentController {
     @Autowired
     private ProjectRepository projectRepo;
 
-    @PostMapping("/newStudent")
+    @PostMapping("/student/newStudent")
     @ResponseBody
     public Student newStudent(@RequestBody Student s){
         return studentRepo.save(s);
     }
 
-    @PutMapping("/timeslot")
+    @PutMapping("/student/timeslot")
     @ResponseBody
     public void changeAvailable(@RequestParam String slot, @RequestParam Long id){
         Student s = studentRepo.findById(id).get();
@@ -26,12 +26,15 @@ public class StudentController {
         studentRepo.save(s);
     }
 
-    @PutMapping("/assignProject")
+    @PutMapping("/student/assignProject")
     public void assignProject(@RequestParam Long studentID, @RequestParam Long projectID){
         if (studentRepo.existsById(studentID) && projectRepo.existsById(projectID)){
             Student s = studentRepo.findById(studentID).get();
             Project p = projectRepo.findById(projectID).get();
             s.setProject(p);
+        }
+        else{
+            //not quite sure what to put in for else but oops something went wrong or smth
         }
     }
 }
