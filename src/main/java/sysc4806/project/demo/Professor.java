ae2e21bbf09fdafdf0e3b157187cd69cebc28a4f
jstiy <jsti.yeh@gmail..com>
2023-11-25 21:16:03 -0500

    add cascading

--- src/main/java/sysc4806/project/demo/Professor.java
+++ src/main/java/sysc4806/project/demo/Professor.java
@@ -3,10 +3,7 @@ package sysc4806.project.demo;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 
 import java.util.List;
 
@@ -18,7 +15,7 @@ public class Professor {
     private String name;
     private String availability;
     @JsonManagedReference
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Project> projects;
 
     public Professor(String name, String availability){
