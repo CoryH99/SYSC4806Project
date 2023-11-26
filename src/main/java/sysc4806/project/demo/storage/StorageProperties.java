dbf5bf403762b612ece549a3867e981d070e9bcc
Jackie Zhuang <jackie_prime@hotmail.ca>
2023-11-25 23:38:43 -0500

    Added project view controller to view individual projects. Added backend for report submission

---
+++ src/main/java/sysc4806/project/demo/storage/StorageProperties.java
@@ -0,0 +1,23 @@
+package sysc4806.project.demo.storage;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties("storage")
+@Component
+public class StorageProperties {
+
+    /**
+     * Folder location for storing files
+     */
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+}
